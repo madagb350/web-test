20bddc4dcc2982b80ec4d1ce4d6acc8a4ebdd410
MAKITA\diado <makitagb350@gmail.com>
2023-08-09 21:17:50 +0900

    ファーストコミット

---
+++ lang.js
@@ -0,0 +1,60 @@
+// 日本語に切り替え
+function langJa(){
+    main1.innerHTML = "ブランドのストーリーテリングとソーシャル メディアでビジネスを成長させましょう。";
+    main2.innerHTML = "私たちは、ビジネスやアーティストが<br>高品質のコンテンツで目立つよう支援します。";
+    btn1.innerHTML = "無料カウンセリングを予約する";
+
+    S1.innerHTML = "カウンセリング";
+    S1_1.innerHTML = "無料の予約を入れて、カスタムメイドのコンテンツがあなたのビジネスにどのようなメリットをもたらす確認してください。<br>  <br> "
+    
+    S2.innerHTML = "ブランドストーリーテリング"
+    S2_1.innerHTML = "実証済みの方法論とチャネる戦略を使用して、本物のストーリーを通じて視聴者とつながりエンゲージメントを高めるお手伝いをします。<br> <br>"
+    
+    S3.innerHTML = "ソーシャルメディアマーケティング"
+    S3_1.innerHTML = "ソーシャルメディア上での関連性と活動を維持するのはフルタイムの仕事です。そのため、当社はすべてのクライアントにコンテンツ作成の継続的なストリームを提供します。"
+    
+    S4.innerHTML = "お問い合わせ"
+
+
+    key.innerHTML = "ペースの速い世界で成功するための鍵";
+    key1.innerHTML = "消費者は無限の選択肢にさらされています。魅力的なブランドストーリーを伝えて目立ち、顧客との感情的なつながりを築き、<br>信頼を築き、競合他社との差別化を図ります。販売するものだけではなく、その背後にあるストーリーも重要です。<br>競争に取り残されないようにブランドのストーリーテリングの力を活用して、ビジネスの成功を見守りましょう。";
+    btn2.innerHTML = "こんにちは";
+
+    btn3.innerHTML = "インスタグラムへアクセス";
+
+    About.innerHTML ="私たちに関して";
+    About1.innerHTML = "私たちの戦略は、行動する前に考えることです。<br>無料コンサルティングでは、貴社の背景と目標を理解するために協力します。<br>そこから、本物のストーリーテリングを通じて視聴者とつながるための戦略を提案し、<br>アクションを起こします。";
+}
+
+
+
+// 英語に切り替え
+function langEn(){
+    main1.innerHTML = "Grow your business with brand<br> storytelling and social media.";
+    main2.innerHTML = "We help business and artists stand out<br>with quality content.";
+    btn1.innerHTML = "BOOK A FREE COUNSELING";
+
+    S1.innerHTML = "counselling";
+    S1_1.innerHTML = "Schedule a compimentary<br>appoitment to discover how our<br>custom-made content can benefit<br>your business."
+    
+    S2.innerHTML = "Brand Storytelling"
+    S2_1.innerHTML = "Using a proven methodlogy and<br>channel strategy, we help you<br>connect with your audience and engage with them through genuine<br>stories."
+   
+    S3.innerHTML = "Social Media Marketing"
+    S3_1.innerHTML = "Maintaining relevance and activity on<br>social media is a full-time job. That's <br>why we provide a constant stream of<br>content creation for all clients."
+
+    S4.innerHTML = "CONTACT US"
+
+
+
+
+
+    key.innerHTML = "The key to thriving in a fast-paced world";
+    key1.innerHTML = "Consumers are bombarded with endless options.Stand out by telling a compelling brand story and create an emotional<br> connection with customers, build trust, and set your business apart from the competition. It's not just about what you sell,<br>but the story behind it. Don't get left behind in the race, harness the power of brand storytelling and watch your business <br>thrive!";
+    btn2.innerHTML = "SAY HELLO";
+
+    btn3.innerHTML = "VISIT OUR INSTAGRAM";
+
+    About.innerHTML ="About us";
+    About1.innerHTML = "Our strategy is to think before we act. During a free<br>consultation, we'll collaborate to understand your company's<br>background and goals. From there, we'll propose a strategy and<br>take action to help you connect with your audience through<br>authentic storytelling.";
+}
